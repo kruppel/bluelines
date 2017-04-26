b9d19a1898dc9152b6f7261d9c9b42fc4c14eebe
Kurt Ruppel <me@kurtruppel.com>
2017-04-26 07:49:02 -0700

    Allow webpacker option

--- rails/app/template.rb
+++
@@ -1,2 +0,0 @@
-copy_file 'app/assets/stylesheets/application.css', force: true
-copy_file 'app/assets/javascripts/application.js', force: true
--- rails/template.rb
+++ rails/template.rb
@@ -12,21 +12,7 @@ template 'Gemfile.tt', force: true
 template 'Rakefile.tt', force: true
 template 'README.md.tt', force: true
 
-apply 'app/template.rb'
 apply 'config/template.rb'
-
-environment %Q{
-  config.generators do |generators|
-    generators.helper       false
-    generators.assets       false
-    generators.javascripts  false
-    generators.stylesheets  false
-    generators.view_specs   false
-    generators.helper_specs false
-
-    generators.test_framework :rspec, fixture: true
-  end
-}
 
 remove_dir 'test'
 generate 'rspec:install'
