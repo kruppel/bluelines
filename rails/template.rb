4557f7fb2415bfd4e81176ed32bce6eb55602efa
Kurt Ruppel <me@kurtruppel.com>
2018-09-23 10:57:43 -0700

    Fix precendence for template in `source_paths`

--- rails/template.rb
+++ rails/template.rb
@@ -1,14 +1,14 @@
 def source_paths
-  Array(super) + [File.expand_path(File.dirname(__FILE__))]
+  [File.expand_path(File.dirname(__FILE__)), *super]
 end
 
 copy_file 'docker-compose.yml', 'docker-compose.yml'
 copy_file 'rubocop.yml', '.rubocop.yml'
-create_file '.ruby-version', RUBY_VERSION
+create_file '.ruby-version', "ruby-#{RUBY_VERSION}"
 
 template 'gitignore', '.gitignore', force: true
 template 'Dockerfile.tt', force: true
-template 'Gemfile.tt', force: true
+template 'Gemfile.tt', 'Gemfile', force: true
 template 'Rakefile.tt', force: true
 template 'README.md.tt', force: true
 
